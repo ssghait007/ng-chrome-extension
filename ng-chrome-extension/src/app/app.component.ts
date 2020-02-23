4d6b630b7f5a99a2a103db290e24636b83d820e7
ssghait007 <ssghait.007@gmail.com>
2020-02-23 20:27:43 +0530

    color change of page-CHECKED

--- ng-chrome-extension/src/app/app.component.ts
+++ ng-chrome-extension/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  color: string;
   title = 'ng-chrome-extension';
+
+  colorize() {
+    chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
+      chrome.tabs.executeScript(
+        tabs[0].id,
+        { code: 'document.body.style.backgroundColor = "' + this.color + '";' }
+      );
+    });
+  }
+
+  public updateColor(color: string) {
+    chrome.storage.sync.set({ color});
+  }
+
+  ngOnInit(): void {
+    chrome.storage.sync.get('color', ({ color }) => {
+      this.color = color;
+      console.log('color')
+    });
+  }
 }
--- ng-chrome-extension/src/background.ts
+++ ng-chrome-extension/src/background.ts
@@ -1,6 +1,8 @@
 chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.sync.set({ color: '#3aa757' });
     chrome.webNavigation.onCompleted.addListener(() => {
       chrome.tabs.query({ active: true, currentWindow: true }, ([{ id }]) => {
+        console.log(chrome.tabs,id)
         chrome.pageAction.show(id);
       });
     }, { url: [{ urlMatches: 'google.com' }] });
